779e70b5e375483e054fc7aa5d43e9b2a8988739
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-11-25 08:54:30 -0300

    fixed cors

--- api/src/app.js
+++ api/src/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://chat-app-ezequiel-bamio.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
 }));
